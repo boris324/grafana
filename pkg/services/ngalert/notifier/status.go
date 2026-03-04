8f09301111b01f041db424d86e14f534006ead42
Matt Jacobson <matthew.jacobson@grafana.com>
2026-03-04 17:30:39 -0500

    Replace local am apply with `PrepareConfig`
    - Update SaveCallback signature to receive AlertConfiguration parameter
    - Pass config directly to callback in SaveAlertmanagerConfigurationWithCallback
    - Update GetStatus to use NotificationsConfiguration

--- pkg/services/ngalert/notifier/status.go
+++ pkg/services/ngalert/notifier/status.go
@@ -5,20 +5,23 @@ import (
 	"encoding/json"
 	"fmt"
 
+	alertingNotify "github.com/grafana/alerting/notify"
+
 	apimodels "github.com/grafana/grafana/pkg/services/ngalert/api/tooling/definitions"
 )
 
 // TODO: We no longer do apimodels at this layer, move it to the API.
 func (am *alertmanager) GetStatus(_ context.Context) (apimodels.GettableStatus, error) {
-	config := &apimodels.PostableUserConfig{}
-	status := am.Base.GetStatus() // TODO: This should return a GettableStatus, for now it returns PostableUserConfig.
+	status := am.Base.GetStatus() // TODO: This should probably return a GettableStatus, for now it returns NotificationsConfiguration.
 	if status == nil {
-		return *apimodels.NewGettableStatus(&config.AlertmanagerConfig), nil
+		return *apimodels.NewGettableStatus(&apimodels.PostableApiAlertingConfig{}), nil
 	}
 
-	if err := json.Unmarshal(status, config); err != nil {
+	config := alertingNotify.NotificationsConfiguration{}
+	if err := json.Unmarshal(status, &config); err != nil {
 		return apimodels.GettableStatus{}, fmt.Errorf("unable to unmarshal alertmanager config: %w", err)
 	}
 
-	return *apimodels.NewGettableStatus(&config.AlertmanagerConfig), nil
+	amConfig := NotificationsConfigurationToPostableAPIConfig(config)
+	return *apimodels.NewGettableStatus(&amConfig), nil
 }
